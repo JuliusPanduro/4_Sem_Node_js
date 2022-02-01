7fd7193de670598420016aac01b2cff3691a542c
JuliusPanduro <juli05d6@stud.kea.dk>
2022-02-01 10:51:15 +0100

    ??

---
+++ 0._Intro/0.Type_coercion.js
@@ -0,0 +1,20 @@
+//Const is a constant variable and can't be manipulated
+const name = "Julius";
+
+//var is not a constant variable and can be manipulated
+var age = 28;
+
+console.log("Name: ",name +" "+ "Age: ",age);
+
+//Type coercion
+//Changes a number and a string into a string
+console.log("Age: " +age);
+
+//To seperate strings and numbers add an "," between
+console.log("Age: ",age);
+
+/*
+2 == "2" non strict evaluation, compares simularities to see if it contains the same numbers, but not the same datatype
+2 === "2" strict evaluation, compares strict to see if the datatypes are equals
+*/
+
